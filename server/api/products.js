012c0bcd5ff3eec775cc1ad9eedf1b0d4671b893
CatherineJOnia <catherinejonia@gmail.com>
2023-02-17 21:18:08 -0500

    products api

---
+++ server/api/products.js
@@ -0,0 +1,52 @@
+const router = require("express").Router();
+const { Product } = require("../db");
+
+router.get("/", async (req, res, next) => {
+  try {
+    const allProducts = await Product.findAll();
+    res.json(allProducts);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get("/:productId", async (req, res, next) => {
+  try {
+    res.json(await Product.findByPk(req.params.productId));
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.put("/:productId", async (req, res, next) => {
+  try {
+    const product = await Product.findByPk(req.params.productId);
+    res.send(await product.update(req.body));
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/", async (req, res, next) => {
+  try {
+    const newProduct = await Product.create(req.body);
+    res.json(newProduct);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.delete("/:productId", async (req, res, next) => {
+  try {
+    const deletedProduct = await Product.findByPk(req.params.productId);
+    if (deletedProduct) {
+      await deletedProduct.destroy();
+      res.json(deletedProduct);
+    } else {
+      console.log("nothing to be deleted");
+    }
+  } catch (err) {
+    next(err);
+  }
+});
+module.exports = router;
---
+++ server/api/products.spec.js
@@ -0,0 +1,23 @@
+const { expect } = require("chai");
+const request = require("supertest");
+const {
+  db,
+  models: { Product },
+} = require("../db");
+const seed = require("../../script/seed");
+const app = require("../app");
+
+describe("Product routes", () => {
+  beforeEach(async () => {
+    await seed();
+  });
+
+  describe("/api/products/", () => {
+    it("GET /api/products", async () => {
+      const res = await request(app).get("/api/products").expect(200);
+
+      expect(res.body).to.be.an("array");
+      expect(res.body.length).to.equal(2);
+    });
+  });
+});
