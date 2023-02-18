e85989ceb5e884335821a165eb58a7bf97a09597
CatherineJOnia <catherinejonia@gmail.com>
2023-02-17 21:15:36 -0500

    users api

---
+++ server/api/users.js
@@ -0,0 +1,56 @@
+const router = require("express").Router();
+const { User } = require("../db");
+module.exports = router;
+
+router.get("/", async (req, res, next) => {
+  try {
+    const users = await User.findAll({
+      attributes: ["id", "email", "isAdmin", "firstName", "lastName"],
+    });
+    res.json(users);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get("/:userId", async (req, res, next) => {
+  try {
+    res.json(
+      await User.findByPk(req.params.userId, {
+        attributes: ["id", "email", "isAdmin", "firstName", "lastName"],
+      })
+    );
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post("/", async (req, res, next) => {
+  try {
+    res.send(await User.create(req.body));
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.put("/:userId", async (req, res, next) => {
+  try {
+    const user = await User.findByPk(req.params.userId);
+    res.send(await user.update(req.body));
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.delete("/:userId", async (req, res, next) => {
+  try {
+    await User.destroy({
+      where: {
+        id: req.params.userId,
+      },
+    });
+    res.json(req.params.userId);
+  } catch (error) {
+    next(error);
+  }
+});
