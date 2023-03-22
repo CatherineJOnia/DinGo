9407879886022c9c75ed3247440a2b2153f985d6
CatherineJOnia <catherinejonia@gmail.com>
2023-03-21 20:42:24 -0400

    webpack loaders

--- webpack.config.js
+++ webpack.config.js
@@ -15,39 +15,39 @@ module.exports = {
           presets: ["@babel/preset-env", "@babel/preset-react"],
         },
       },
-      {
-        test: /\.css$/,
-        loader: "style-loader!css-loader",
-      },
-      {
-        test: /\.(gif|png|jpe?g|svg)$/i,
-        use: [
-          "file-loader",
-          {
-            loader: "image-webpack-loader",
-            options: {
-              mozjpeg: {
-                progressive: true,
-              },
-              // optipng.enabled: false will disable optipng
-              optipng: {
-                enabled: false,
-              },
-              pngquant: {
-                quality: [0.65, 0.9],
-                speed: 4,
-              },
-              gifsicle: {
-                interlaced: false,
-              },
-              // the webp option will enable WEBP
-              webp: {
-                quality: 75,
-              },
-            },
-          },
-        ],
-      },
+      // {
+      //   test: /\.css$/,
+      //   loader: "style-loader!css-loader",
+      // },
+      // {
+      //   test: /\.(gif|png|jpe?g|svg)$/i,
+      //   use: [
+      //     "file-loader",
+      //     {
+      //       loader: "image-webpack-loader",
+      //       options: {
+      //         mozjpeg: {
+      //           progressive: true,
+      //         },
+      //         // optipng.enabled: false will disable optipng
+      //         optipng: {
+      //           enabled: false,
+      //         },
+      //         pngquant: {
+      //           quality: [0.65, 0.9],
+      //           speed: 4,
+      //         },
+      //         gifsicle: {
+      //           interlaced: false,
+      //         },
+      //         // the webp option will enable WEBP
+      //         webp: {
+      //           quality: 75,
+      //         },
+      //       },
+      //     },
+      //   ],
+      // },
     ],
   },
 };
